d2de2ac86ad0b9ca852cce05c984a210208fe855
Unknown <alain.cadenat@depannum.com>
2018-03-30 14:29:00 +0200

    Second requirement

--- js/app.js
+++ js/app.js
@@ -1,11 +1,34 @@
 /* Variriable to store the number of clicks */
 let nbClicks = 0;
+class Cat {
+  constructor(name,picture) {
+    this.name = name;
+    this.picture = picture;
+  }
+}
 /* The element where we await clicks */
-let theCat = document.getElementById('cat');
+let theCats = document.getElementsByClassName('cat');
 let clickCounter = document.getElementById('clicks');
 
-theCat.addEventListener('click', function(){
-  //the cat has been clicked
-  nbClicks++;
-  clickCounter.textContent = nbClicks;
-}, false);
+let allCats = [
+  new Cat('tom', 'img/cat.jpg'),
+  new Cat('pussy cat','img/cat-2.jpg')
+];
+
+function addCat(cat){
+  let htmlCat = document.getElementById('cats').innerHTML;
+  htmlCat+=`<span><figure><img class="cat" src=${cat.picture}>
+  <figcaption class="name">${cat.name}<figcaption>
+  </figure></span>`;
+  document.getElementById('cats').innerHTML=htmlCat;
+}
+addCat(allCats[0]);
+addCat(allCats[1]);
+
+for (let cat of theCats) {
+  cat.addEventListener('click', function(){
+    //the cat has been clicked
+    nbClicks++;
+    clickCounter.textContent = nbClicks;
+  }, false);
+}
